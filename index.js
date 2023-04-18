cf00c14e9b35fbbab101bfe26b01d1a8c6ab9b91
Gyudon <gyudoncode@gmail.com>
2023-04-19 00:07:10 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next){
+    // res.json({test: 'hello multer world'})  
+    res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size})
+})
 
 
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Server running on port ' + port)
 });
